b45273645a65cf0bb94fe0ae128c2152bd5be085
Xi Yan <xiyan@meta.com>
2025-04-01 15:26:26 -0700

    Sync updates from stainless branch: main

--- tests/api_resources/test_models.py
+++ tests/api_resources/test_models.py
@@ -9,7 +9,7 @@ import pytest
 
 from llama_api import LlamaAPI, AsyncLlamaAPI
 from tests.utils import assert_matches_type
-from llama_api.types import AIModel, ModelListResponse
+from llama_api.types import LlamaModel, ModelListResponse
 
 base_url = os.environ.get("TEST_API_BASE_URL", "http://127.0.0.1:4010")
 
@@ -23,7 +23,7 @@ class TestModels:
         model = client.models.retrieve(
             "Llama-3.3-70B-Instruct",
         )
-        assert_matches_type(AIModel, model, path=["response"])
+        assert_matches_type(LlamaModel, model, path=["response"])
 
     @pytest.mark.skip()
     @parametrize
@@ -35,7 +35,7 @@ class TestModels:
         assert response.is_closed is True
         assert response.http_request.headers.get("X-Stainless-Lang") == "python"
         model = response.parse()
-        assert_matches_type(AIModel, model, path=["response"])
+        assert_matches_type(LlamaModel, model, path=["response"])
 
     @pytest.mark.skip()
     @parametrize
@@ -47,7 +47,7 @@ class TestModels:
             assert response.http_request.headers.get("X-Stainless-Lang") == "python"
 
             model = response.parse()
-            assert_matches_type(AIModel, model, path=["response"])
+            assert_matches_type(LlamaModel, model, path=["response"])
 
         assert cast(Any, response.is_closed) is True
 
@@ -97,7 +97,7 @@ class TestAsyncModels:
         model = await async_client.models.retrieve(
             "Llama-3.3-70B-Instruct",
         )
-        assert_matches_type(AIModel, model, path=["response"])
+        assert_matches_type(LlamaModel, model, path=["response"])
 
     @pytest.mark.skip()
     @parametrize
@@ -109,7 +109,7 @@ class TestAsyncModels:
         assert response.is_closed is True
         assert response.http_request.headers.get("X-Stainless-Lang") == "python"
         model = await response.parse()
-        assert_matches_type(AIModel, model, path=["response"])
+        assert_matches_type(LlamaModel, model, path=["response"])
 
     @pytest.mark.skip()
     @parametrize
@@ -121,7 +121,7 @@ class TestAsyncModels:
             assert response.http_request.headers.get("X-Stainless-Lang") == "python"
 
             model = await response.parse()
-            assert_matches_type(AIModel, model, path=["response"])
+            assert_matches_type(LlamaModel, model, path=["response"])
 
         assert cast(Any, response.is_closed) is True
 
