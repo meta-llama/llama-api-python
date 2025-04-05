393e408f4a1a96f7b66b166be523c5b68ff1550d
Xi Yan <xiyan@meta.com>
2025-04-04 23:30:05 -0700

    Sync updates from stainless branch: main

--- src/llama_api/types/moderation_create_response.py
+++ src/llama_api/types/moderation_create_response.py
@@ -1,19 +1,19 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
 from typing import List
-from typing_extensions import TypeAlias
 
 from .._models import BaseModel
 
-__all__ = ["ModerationCreateResponse", "ModerationCreateResponseItem"]
+__all__ = ["ModerationCreateResponse", "Result"]
 
 
-class ModerationCreateResponseItem(BaseModel):
+class Result(BaseModel):
     flagged: bool
 
     flagged_categories: List[str]
 
+
+class ModerationCreateResponse(BaseModel):
     model: str
 
-
-ModerationCreateResponse: TypeAlias = List[ModerationCreateResponseItem]
+    results: List[Result]
